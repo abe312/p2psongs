8757cd5107c8927ce97d117ea0781095b4c4e095
abe312 <abhineet312@gmail.com>
2017-06-20 07:18:23 +0530

    updated views, urls, and templates in music app

--- music/urls.py
+++ music/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import include, url
 from . import views
 app_name = 'music'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<album_id>[0-9]{,99999})/$', views.detail, name='detail'),
-    url(r'^(?P<album_id>[0-9]{,99999})/favorite$',
-        views.favorite, name='favorite'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]{,99999})/$', views.DetailView.as_view(), name='detail'),
+
 ]
---
+++ music/views.py
@@ -0,0 +1,14 @@
+from django.views import generic
+from .models import Album, Song
+
+
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+
+    def get_queryset(self):
+        return Album.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
